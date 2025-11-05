ba70e0e54997abe6396420433bb68e74a0b1549e
gimenesjulia-arch <gimenes.julia@uni9.edu.br>
2025-11-05 11:33:02 -0300

    Implement VideoMessage class
    Added VideoMessage class inheriting from MediaMessage.

--- VideoMessage.cs
+++ VideoMessage.cs
@@ -1,1 +1,25 @@
+public class MessageBase
+{
+    public string Message { get; set; }
+
+    public MessageBase(string message)
+    {
+        Message = message;
+    }
+}
+
+public class MediaMessage : MessageBase
+{
+    public string File { get; set; }
+    public string Format { get; set; }
+
+    public MediaMessage(string message) : base(message) { }
+}
+
+public class VideoMessage : MediaMessage
+{
+    public int Duration { get; set; }  // duração em segundos
+
+    public VideoMessage(string message) : base(message) { }
+}
 
