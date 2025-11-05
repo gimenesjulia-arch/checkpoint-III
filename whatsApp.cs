38cdcb8a42efe2b76d7faaf65c540f32e70194cf
gimenesjulia-arch <gimenes.julia@uni9.edu.br>
2025-11-05 11:06:26 -0300

    Atualizei o método de envio de mensagens no whatsApp

--- whatsApp.cs
+++ whatsApp.cs
@@ -1,21 +1,30 @@
 public class WhatsApp
 {
-    public void Enviarmensagem(string numero, TextMessage message)
+    public void EnviarMensagem(string numero, TextMessage message)
     {
         Console.WriteLine($"Enviando mensagem de texto para o número {numero}");
-    } 
+        Console.WriteLine($"Mensagem: {message.Message}");
 
-      
-public void Enviarmensagem(string numero, VideoMessage message)
+    }
+
+    public void EnviarMensagem(string numero, VideoMessage message)
     {
         Console.WriteLine($"Enviando mensagem de video para o número {numero}");
-    } 
-public void Enviarmensagem(string numero, PhotoMessage message)
+                Console.WriteLine($"Mensagem: {message.Message}");
+
+    }
+
+    public void EnviarMensagem(string numero, PhotoMessage message)
     {
         Console.WriteLine($"Enviando mensagem de foto para o número {numero}");
-    }    
+                Console.WriteLine($"Mensagem: {message.Message}");
 
-    public void Enviarmensagem(string numero, FileMessage message)
+    }
+
+     public void EnviarMensagem(string numero, FileMessage message)
     {
         Console.WriteLine($"Enviando mensagem de arquivo para o número {numero}");
-    } 
+                Console.WriteLine($"Mensagem: {message.Message}");
+
+    }
+}
