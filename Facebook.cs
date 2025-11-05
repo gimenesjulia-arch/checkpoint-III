eb6fa9a3d126cadeb1d2880336ebf4923cc8d2f5
gimenesjulia-arch <gimenes.julia@uni9.edu.br>
2025-11-05 20:03:50 -0300

    Add methods to send different message types on Facebook

--- Facebook.cs
+++ Facebook.cs
@@ -1,1 +1,31 @@
+using System;
+
+public class Facebook
+{
+    public void EnviarMensagem(string usuario, TextMessage message)
+    {
+        Console.WriteLine($"Enviando mensagem de texto para o usuário {usuario} no Facebook");
+        Console.WriteLine($"Mensagem: {message.Message}");
+    }
+
+    public void EnviarMensagem(string usuario, VideoMessage message)
+    {
+        Console.WriteLine($"Enviando mensagem de vídeo para o usuário {usuario} no Facebook");
+        Console.WriteLine($"Mensagem: {message.Message}");
+        Console.WriteLine($"Arquivo: {message.File}");
+        Console.WriteLine($"Formato: {message.Format}");
+        Console.WriteLine($"Duração: {message.Duration} segundos");
+    }
+
+    public void EnviarMensagem(string usuario, PhotoMessage message)
+    {
+        Console.WriteLine($"Enviando mensagem de foto para o usuário {usuario} no Facebook");
+        Console.WriteLine($"Mensagem: {message.Message}");
+        Console.WriteLine($"Arquivo: {message.File}");
+        Console.WriteLine($"Formato: {message.Format}");
+    }
+
+    public void EnviarMensagem(string usuario, FileMessage message)
+    {
+        Console.WriteLine($"Enviando mensagem de arquivo para o us
 
