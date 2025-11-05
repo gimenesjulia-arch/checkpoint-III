dcc8844c0cab2fedfdc42d26774665e27a5f89a6
gimenesjulia-arch <gimenes.julia@uni9.edu.br>
2025-11-05 20:01:59 -0300

    Add message classes for Telegram API

--- Telegram.cs
+++ Telegram.cs
@@ -1,1 +1,28 @@
+public class TextMessage
+{
+    public string Message { get; set; }
+    public DateTime DataEnvio { get; set; } = DateTime.Now;
+}
+
+public class VideoMessage
+{
+    public string Message { get; set; }
+    public string File { get; set; }
+    public string Format { get; set; }
+    public int Duration { get; set; }
+}
+
+public class PhotoMessage
+{
+    public string Message { get; set; }
+    public string File { get; set; }
+    public string Format { get; set; }
+}
+
+public class FileMessage
+{
+    public string Message { get; set; }
+    public string File { get; set; }
+    public string Format { get; set; }
+}
 
