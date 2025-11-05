ee4ea963f03f16d56853b957041d44205c185547
gimenesjulia-arch <gimenes.julia@uni9.edu.br>
2025-11-05 11:39:01 -0300

    Add FileMessage class for file handling

--- FileMessage.cs
+++ FileMessage.cs
@@ -1,1 +1,11 @@
+public class FileMessage : MessageBase
+{
+    public string File { get; set; }       // Caminho ou nome do arquivo
+    public string Format { get; set; }     // Tipo de arquivo (ex: "pdf", "docx", "zip")
+
+    public FileMessage(string message)
+        : base(message)
+    {
+    }
+}
 
