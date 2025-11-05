565345e625def28f096f87afbb2b7425a4c591c5
gimenesjulia-arch <gimenes.julia@uni9.edu.br>
2025-11-05 11:09:40 -0300

    Fix input validation for channel selection

--- app/Program.cs
+++ app/Program.cs
@@ -1,17 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Para qual canal você quer enviar mensagem?");
-Console.WriteLine("1.  WhatsApp");
-Console.WriteLine("2. Telegram");  
-Console.WriteLine("3. Instagram");  
+Console.WriteLine("1. WhatsApp");
+Console.WriteLine("2. Telegram");
+Console.WriteLine("3. Instagram");
 Console.WriteLine("4. Facebook");
 
 int canal = 0;
 string resposta = Console.ReadLine();
-if (int.TryParse(resposta, out canal))
+if (!int.TryParse(resposta, out canal))
 {
-  Console.WriteLine("Opção inválida");
-  return;       
-}   
+    Console.WriteLine("Opção inválida");
+    return;
+}
 
-TextMessage textMessage = new TextMessage("Olá);
+TextMessage messageBase = new TextMessage("Olá");
 VideoMessage video = new VideoMessage("Olá");
+if(canal == 1)
+{
+    var whats = new WhatsApp();
+    whats.EnviarMensagem("12345678", messageBase);
+
+    whats.EnviarMensagem("87654321", video);
+}
+
+
